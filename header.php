6516a969447f2b3171a5e80b2af7ec18057e64a1
Samar Aljumaa <s.aljumaa@gmail.com>
2016-03-08 17:22:23 -0500

    Initial commit

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<?php // Load Meta ?>
+  <meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href='https://fonts.googleapis.com/css?family=Playfair+Display:700|Open+Sans:400,700' rel='stylesheet' type='text/css'>
+  <title><?php  wp_title('|', true, 'right'); ?></title>
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+  <!-- stylesheets should be enqueued in functions.php -->
+  <?php wp_head(); ?>
+</head>
+
+
+<body <?php body_class(); ?>>
+
+<header>
+  <div class="container">
+    <h1>
+      <a href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo( 'name', 'display' ); ?>" rel="home">
+        <?php bloginfo( 'name' ); ?>
+      </a>
+    </h1>
+
+    <?php wp_nav_menu( array(
+      'container' => false,
+      'theme_location' => 'primary'
+    )); ?>
+  </div> <!-- /.container -->
+</header><!--/.header-->
+
